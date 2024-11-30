9ff301515e9866a620da281bfe4724bbc584f0b6
jpxzn <joaopzaidman@aluno.puc-rio.br>
2024-11-29 23:39:18 -0300

    sla

--- cria_func.c
+++ cria_func.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cria_func.h"
 
-void preencheVetorEndereço(void* f,unsigned char codigo[], int* indice)
+void preencheHexaPtr(void* f, unsigned char codigo[], int* indice)
 {
     unsigned long int endLong = (unsigned long int)f; 
 
+    printf("%x\n",endLong);
     for(int i = 0; i < 8; i++)
-    {
-        codigo[i] = endLong & 0xff; 
-        endLong >>= 8;
+    {     
+        codigo[*(indice)] = endLong & 0xff;
         *(indice)++;
+        endLong >>= 8;
+
     }
+}
+
+void preencheHexaInt(int valor, unsigned char codigo[], int* indice){
+    for(int i = 0; i < 4; i++)
+    {
+       codigo[*(indice)++] = valor & 0xff;
+       valor >>= 8;
+    } 
 }
 
 void cria_func (void* f, DescParam params[], int n, unsigned char codigo[])
@@ -19,7 +30,9 @@ void cria_func (void* f, DescParam params[], int n, unsigned char codigo[])
         fprintf(stderr, "Foram recebidos %d parametros. Apenas sao aceitos entre 1 a 3 parametros!\n", n);
         exit(1);
     }
+
     int indice = 0;
+
 //parte fixa
 
     //pushq %rbp
@@ -28,13 +41,110 @@ void cria_func (void* f, DescParam params[], int n, unsigned char codigo[])
     //movq %rsp,%rbp
     codigo[indice++] = 0x48; 
     codigo[indice++] = 0x89; 
-    codigo[indice++] = 0xe5; 
+    codigo[indice++] = 0xe5;
+
+    //subq $32,%rsp
+    codigo[indice++] = 0x48; 
+    codigo[indice++] = 0x89;
+    codigo[indice++] = 0xec;
+    codigo[indice++] = 0x20;
+
+    //movq %rdi, -8(%rbp)
+    codigo[indice++] = 0x48; 
+    codigo[indice++] = 0x89;
+    codigo[indice++] = 0x7d;
+    codigo[indice++] = 0xf8;
+
+    //movq %rsi, -16(%rbp)
+    codigo[indice++] = 0x48; 
+    codigo[indice++] = 0x89;
+    codigo[indice++] = 0x75;
+    codigo[indice++] = 0xf0;
+
+    //movq %rdx, -32(%rbp)
+    codigo[indice++] = 0x48; 
+    codigo[indice++] = 0x89;
+    codigo[indice++] = 0x55;
+    codigo[indice++] = 0xe8;
 
 //parte variante
 
+    int qtdParams = 0;
 
+    for(int i = 0; i < n;i++)
+    {
+        if(params[i].orig_val==FIX)
+        {
+            unsigned char regs[3] = {0xbf,0xbe,0xba};
+            
+            if(params[i].tipo_val==PTR_PAR)
+            {
+                //movq $endereço, %registardorParametro
+                codigo[indice++] = 0x48;
+                codigo[indice++] = regs[i];
+                preencheHexaPtr(params[i].valor.v_ptr, codigo, &indice);
+            } 
+            else if(params[i].tipo_val==INT_PAR)
+            {
+                //movl $valorInt, %registardorParametro
+                codigo[indice++] = regs[i];
+                preencheHexaInt(params[i].valor.v_int, codigo, &indice);
+            }
+        }
 
+        else if(params[i].orig_val==IND)
+        {
+            unsigned char regs[3] = {0x39,0x31,0x11};
 
+            if(params[i].tipo_val==PTR_PAR)
+            {
+                //movq $endereço, %rcx
+                codigo[indice++] = 0x48; 
+                codigo[indice++] = 0xb9;
+                preencheHexaPtr(params[i].valor.v_ptr, codigo, &indice);
+
+                //movq (%rcx), %registardorParametro
+                codigo[indice++] = 0x48;
+                codigo[indice++] = 0x8b;
+                codigo[indice++] = regs[i];
+            }
+            else if(params[i].tipo_val==INT_PAR)
+            {
+                //movq $endereço, %rcx
+                codigo[indice++] = 0x48; 
+                codigo[indice++] = 0xb9;
+                preencheHexaInt(params[i].valor.v_int, codigo, &indice);
+
+                //movl (%rcx), %registardorParametro
+                codigo[indice++] = 0x8b;
+                codigo[indice++] = regs[i];
+            }
+        }
+
+        if(params[i].orig_val==PARAM)
+        {
+            unsigned char origem[3] = {0xf8, 0xf0,0xe8};
+            unsigned char destino[3] = {0x7d, 0x75, 0x55};
+            if(params[i].tipo_val==INT_PAR)
+            {
+                //movl -valor(%rbp), %registradorParametro
+                codigo[indice++] = 0x8b;
+                codigo[indice++] = destino[i];
+                codigo[indice++] = origem[qtdParams];
+
+            }
+
+            else if(params[i].tipo_val==PTR_PAR)
+            {
+                codigo[indice++] = 0x48;
+                codigo[indice++] = 0x8b;
+                codigo[indice++] = destino[i];
+                codigo[indice++] = origem[qtdParams];
+
+            }
+            qtdParams++;
+        }
+    }
 
 //parte fixa 
 
@@ -42,11 +152,11 @@ void cria_func (void* f, DescParam params[], int n, unsigned char codigo[])
     // movq $endereço, %rax
     codigo[indice++] = 0x48; 
     codigo[indice++] = 0xb8;
-    preencheVetorEndereço(f, codigo[indice], &indice); 
+    preencheHexaPtr(f, codigo, &indice); 
     
     //chama função e retorna 
     codigo[indice++] = 0xff; codigo[indice++] = 0xd0; //call *%rax
+
     codigo[indice++] = 0xc9; //leave
     codigo[indice++] = 0xc3; //ret
-
 }
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "cria_func.h"
 
-typedef int (*func_ptr) (int x);
+typedef int (*func_ptr) ();
 
 int mult(int x, int y) {
   return x * y;
@@ -13,19 +13,21 @@ int main (void) {
   int i;
   unsigned char codigo[500];
 
-  params[0].tipo_val = INT_PAR; /* o primeiro parãmetro de mult é int */
-  params[0].orig_val = PARAM;   /* a nova função repassa seu parämetro */
+  params[0].tipo_val = INT_PAR; /* a nova função passa para mult um valor inteiro */
+  params[0].orig_val = IND;     /* que é o valor corrente da variavel i */
+  params[0].valor.v_ptr = &i;
 
-  params[1].tipo_val = INT_PAR; /* o segundo parâmetro de mult é int */
-  params[1].orig_val = FIX;     /* a nova função passa para mult a constante 10 */
+  params[1].tipo_val = INT_PAR; /* o segundo argumento passado para mult é a constante 10 */
+  params[1].orig_val = FIX;
   params[1].valor.v_int = 10;
 
   cria_func (mult, params, 2, codigo);
-  f_mult = (func_ptr) codigo;   
+  f_mult = (func_ptr) codigo;
 
   for (i = 1; i <=10; i++) {
-    printf("%d\n", f_mult(i)); /* a nova função só recebe um argumento */
+    printf("%d\n", f_mult()); /* a nova função não recebe argumentos */
   }
 
+  //libera_func(f_mult); <== removido
   return 0;
 }
